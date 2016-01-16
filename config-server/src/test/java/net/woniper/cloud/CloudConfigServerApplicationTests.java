11cd18d8d5dc51ccdee60718a5b8f821adbf54e5
woniper <leekw3747@gmail.com>
2016-01-16 23:50:41 +0900

    crete project

---
+++ config-server/src/test/java/net/woniper/cloud/CloudConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package net.woniper.cloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CloudConfigServerApplication.class)
+public class CloudConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
